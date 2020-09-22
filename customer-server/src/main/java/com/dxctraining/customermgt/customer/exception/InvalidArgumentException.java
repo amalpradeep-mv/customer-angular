cc6f4dd1770a66307db9bec11ac734085cfce7b9
amalpradeep-mv <amalpradeepmv@gmail.com>
2020-09-22 21:59:25 +0530

    deletd

--- customer-server/src/main/java/com/dxctraining/customermgt/customer/exception/InvalidArgumentException.java
+++
@@ -1,8 +0,0 @@
-package com.dxctraining.customermgt.customer.exception;
-
-public class InvalidArgumentException extends RuntimeException{
-
-		public InvalidArgumentException(String msg) {
-			super(msg);
-		}
-}
